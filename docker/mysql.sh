fe9c9d856844357e00a094825edc6c0a42abb087
koichi tada <kou26haru@gmail.com>
2019-03-25 23:56:22 +0900

    modify: add docker-compose for mysql

--- docker/mysql.sh
+++
@@ -1,8 +0,0 @@
-docker run -d \
-  -v $PWD/db:/var/lib/mysql \
-  -v $PWD/init:/docker-entrypoint-initdb.d \
-  -e MYSQL_ROOT_PASSWORD=password \
-  -e MYSQL_USER=django \
-  -e MYSQL_PASSWORD=password \
-  -e MYSQL_DATABASE=django_tutorial \
-  -p 3306:3306 --name mysql mysql:5.7
